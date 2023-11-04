fedf4c62f5b64cf46fc386c5d97d4f3772d95f10
manick02 <manickavasagam.sundaram@gmail.com>
2023-11-04 10:31:59 +0530

    Morse code cample from antlr4

---
+++ hello-world/src/main/java/org/example/MorseCodeParser/MorseCodeAntlrExample.java
@@ -0,0 +1,27 @@
+package org.example.MorseCodeParser;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.TokenStream;
+import org.cobol.Cobol85Parser;
+import org.cobol.morsecodeLexer;
+import org.example.CobolLexerExample;
+
+import java.io.InputStream;
+
+public class MorseCodeAntlrExample {
+
+    public static void main(String[] args) {
+        try {
+            InputStream inputStream = CobolLexerExample.class.getResourceAsStream("/morse_example.txt");
+            Lexer lexer = new morsecodeLexer(CharStreams.fromStream(inputStream));
+            TokenStream tokenStream = new CommonTokenStream(lexer);
+            Cobol85Parser parser = new Cobol85Parser(tokenStream);
+
+        } catch (Exception ex) {
+
+            System.out.println("Hello how are you");
+        }
+    }
+}
