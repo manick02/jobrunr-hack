4f9e612be1617330bf3f8996de52e977730da8f0
manick02 <manickavasagam.sundaram@gmail.com>
2023-11-16 12:03:03 +0530

    Different type of hashmap

--- hello-world/src/main/java/org/example/ConcurrentSkipListSpike.java
+++ hello-world/src/main/java/org/example/ConcurrentSkipListSpike.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.NavigableSet;
 import java.util.concurrent.ConcurrentSkipListMap;
 
@@ -7,10 +10,20 @@ public class ConcurrentSkipListSpike {
 
     public static void main(String[] args) {
         ConcurrentSkipListMap<Long,String> skipListMap = new ConcurrentSkipListMap<>();
-        skipListMap.put(1l,"First");
-        skipListMap.put(2l,"Second");
-        NavigableSet<Long> longs = skipListMap.navigableKeySet();
+        Map<Long,String>  hashMap = new HashMap<>();
+        Map<Long,String> linkedHashMap = new LinkedHashMap<>();
+        generateMockData(skipListMap);
+        generateMockData(hashMap);
+        generateMockData(linkedHashMap);
 //        longs.ceiling();
         System.out.println(skipListMap);
+        System.out.println(hashMap);
+        System.out.println(linkedHashMap);
+    }
+
+    private static void generateMockData(Map<Long, String> map) {
+        map.put(2l,"Second");
+        map.put(1l,"First");
+
     }
 }
