ccea764e077dac40372f461be80921454618cb24
manick02 <manickavasagam.sundaram@gmail.com>
2023-11-17 14:34:22 +0530

    Implementing doubly linked list

--- hello-world/src/main/java/org/example/AllOneDs.java
+++ hello-world/src/main/java/org/example/AllOneDs.java
@@ -1,19 +1,43 @@
 package org.example;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class AllOneDs {
-    private final LinkedHashMap<String,Integer> map = new LinkedHashMap<>(1000,.75f,true);
+    static class Node {
+        Set<String> keys;
+        Node prev;
+        Node next;
+
+        public Node() {
+            this.keys = new HashSet<>();
+        }
+
+        public static void addAfter(Node node, Node newNode) {
+            Node temp = node.next;
+            newNode.prev = node;
+            newNode.next = temp;
+            node.next = newNode;
+            if (temp != null) {
+                temp.prev = newNode;
+            }
+        }
+    }
+
+    private final LinkedHashMap<String, Integer> map = new LinkedHashMap<>(1000, .75f, true);
+
     public AllOneDs() {
 
     }
+
     public void inc(String key) {
         if (map.containsKey(key)) {
             Integer count = map.get(key);
-            map.put("key",count+1);
+            map.put("key", count + 1);
         } else {
-            map.put(key,0);
+            map.put(key, 0);
         }
     }
 
--- hello-world/src/test/java/org/example/AllOneDsTest.java
+++ hello-world/src/test/java/org/example/AllOneDsTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 // leetcode https://leetcode.com/problems/all-oone-data-structure/solutions/3963653/python-hashmap-doubly-linked-list/
+// https://leetcode.com/problems/all-oone-data-structure/solutions/731468/hashmap-doublylinkedlist-strategy/
 public class AllOneDsTest {
     @Test
     public void allOneInstanceCreation() {
@@ -23,5 +24,26 @@ public class AllOneDsTest {
         ds.inc("key1");
         String maxKey =  ds.getMaxKey();
         assertEquals(maxKey,"key2");
+    }
+
+    @Test
+    public void allOneInstanceNodeConstruction() {
+        AllOneDs.Node node = new AllOneDs.Node();
+        assertNotNull(node);
+    }
+
+    @Test
+    public void addNodeafter() {
+        AllOneDs.Node node = new AllOneDs.Node();
+        node.keys.add("Hello");
+        AllOneDs.Node after = new AllOneDs.Node();
+        after.keys.add("world");
+        AllOneDs.Node.addAfter(node,after);
+        assertNotNull(node.next);
+        assertNull(node.prev);
+        assertNotNull(after.prev);
+        assertNull(after.next);
+        assertEquals(node,node.next.prev);
+        assertEquals(after,after.prev.next);
     }
 }
