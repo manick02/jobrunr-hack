f8f54b0e3044d44c27bdc39b5c5afdc41b05601f
manick02 <manickavasagam.sundaram@gmail.com>
2023-11-20 10:19:25 +0530

    LFUCache Implementation with not optimised evict

--- hello-world/src/main/java/org/example/LFUCache.java
+++ hello-world/src/main/java/org/example/LFUCache.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * LFU Cache implementation without using LinkedHashMap
@@ -11,7 +9,9 @@ import java.util.Map;
 public class LFUCache {
     private static final int  defaultSize = 100;
     Map<String,Object> map = new HashMap<>();
+    Map<String,Integer> keyAccessCount = new HashMap<>();
     private int size = 0;
+    private int currentSize = 0;
 
     /**
      * 
@@ -25,10 +25,29 @@ public class LFUCache {
     }
 
     public void put(String key, Object value){
+    if (this.map.size() == size) {
+        evictLessFrequentKey(); // this is o(n) for insert and does not know what key to evict in case of lot of keys have same minn freq
+    }
+
      map.put(key,value);
+     keyAccessCount.put(key,0);
+
+    }
+
+    private void evictLessFrequentKey() {
+        Map.Entry<String, Integer> min = Collections.min(this.keyAccessCount.entrySet(), Map.Entry.comparingByValue());
+        String key = min.getKey();
+        this.keyAccessCount.remove(key);
+        this.map.remove(key);
     }
 
     public Object get(String key) {
+        Integer count = this.keyAccessCount.get(key);
+        if (count == null)
+            return null;
+        this.keyAccessCount.put(key,count+1);
         return map.get(key);
     }
+
+
 }
--- hello-world/src/test/java/org/example/LFUCacheTest.java
+++ hello-world/src/test/java/org/example/LFUCacheTest.java
@@ -30,7 +30,7 @@ public class LFUCacheTest {
     }
 
     @Test
-    void putAndGetLimitWithFixedCacheSize() {
+    void putAndGetLimitWithFixedCacheSizeOfOne() {
         int size = 1;
         LFUCache cache = new LFUCache(size);
         Object obj1 = new Object();
@@ -42,5 +42,25 @@ public class LFUCacheTest {
 
         assertNull(cache.get("Hello"));
         assertEquals(obj2, cache.get("World"));
+
+        Object obj3 = new Object();
+        cache.put("HelloWorld", obj3);
+        assertNull(cache.get("World"));
+    }
+
+    @Test
+    void putAndGetLimitWithFixedCacheSizeOfTwo() {
+        int size = 2;
+        LFUCache cache = new LFUCache(size);
+        Object obj1 = new Object();
+        cache.put("Hello", obj1);
+
+        Object obj2 = new Object();
+        cache.put("World", obj2);
+
+
+        Object obj3 = new Object();
+        cache.put("HelloWorld", obj3);
+        assertNull(cache.get("World")); // assertNull(cache.get("World"))
     }
 }
