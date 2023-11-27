c7e7d68a718789e2a058c2a4b64d79168a4043e6
manick02 <manickavasagam.sundaram@gmail.com>
2023-11-27 12:26:43 +0530

    maximise minimum of segment sums

---
+++ hello-world/src/main/java/org/example/problems/SegementArrays2.java
@@ -0,0 +1,9 @@
+package org.example.problems;
+
+/**
+ * Segment Arrays such that sum of minimums is maximised
+ * https://www.geeksforgeeks.org/divide-the-array-in-k-segments-such-that-the-sum-of-minimums-is-maximized/
+ *
+ */
+public class SegementArrays2 {
+}
