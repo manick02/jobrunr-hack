cdc000c5f27b754fd5a23bc70a2eef7dc3d5e008
manick02 <manickavasagam.sundaram@gmail.com>
2023-12-01 10:26:43 +0530

    Contains and Might Contain

--- hello-world/src/main/java/org/example/lsm/bloom/BloomFilter.java
+++ hello-world/src/main/java/org/example/lsm/bloom/BloomFilter.java
@@ -1,21 +1,49 @@
 package org.example.lsm.bloom;
 
+import org.apache.commons.codec.digest.MurmurHash3;
+
+import static java.lang.Math.ceil;
 import static java.lang.Math.log;
 
 public class BloomFilter {
 
     private final int size;
+    private final int hashCount;
+    private final long[] bits;
 
     public BloomFilter(int expectedInsertion, float falsePositiveTolerance) {
-       this.size = (int)(-expectedInsertion * log(falsePositiveTolerance)/(log(2)*log(2)));
+        this.size = (int) (-expectedInsertion * log(falsePositiveTolerance) / (log(2) * log(2)));
+        this.hashCount = (int) ceil(-log(falsePositiveTolerance) / log(2));
+        this.bits = new long[(int) ceil(size / 64.0)];
     }
 
-    public void add(byte[] key){
+    public void add(byte[] key) {
 
+        long[] hash = getHash(key);
+        long h1 = hash[0], h2 = hash[1];
+
+        for (int i = 0; i < hashCount; i++) {
+            int bit = (int) Math.abs((h1 + i * h2) % size);
+            bits[bit / 64] |= 1L << (bit % 64);
+
+        }
+
+    }
+
+    private long[] getHash(byte[] key) {
+        long[] hashes = MurmurHash3.hash128x64(key, 0, key.length, 0);
+        return hashes;
     }
 
     public boolean mightContain(byte[] key) {
-        return false;
+        long[] hash = getHash(key);
+        long h1 = hash[0], h2 = hash[1];
+        for(int i = 0; i < hashCount; i++) {
+            int bit = (int) Math.abs((h1 + i * h2) % size);
+            if ((bits[bit / 64] & (1L << (bit % 64))) == 0)
+                return false;
+        }
+        return true;
     }
 
     public static BloomFilter readFromFile(String fileName) {
--- hello-world/src/test/java/org/example/lsm/bloom/BloomFilterTest.java
+++ hello-world/src/test/java/org/example/lsm/bloom/BloomFilterTest.java
@@ -13,4 +13,28 @@ public class BloomFilterTest {
         BloomFilter bloomFilter = new BloomFilter(expectedInsertion,falsePositiveTolerance);
         assertNotNull(bloomFilter);
     }
+
+    @Test
+    public void addKeyToBloomFilter() {
+        int expectedInsertion = 10000;
+        float falsePositiveTolerance = 0.001f;
+        BloomFilter bloomFilter = new BloomFilter(expectedInsertion,falsePositiveTolerance);
+        String key = "HelloWorld";
+        bloomFilter.add(key.getBytes());
+    }
+
+    @Test
+    public void checkIfKeyPresentInBloomFilter() {
+        int expectedInsertion = 10000;
+        float falsePositiveTolerance = 0.001f;
+        BloomFilter bloomFilter = new BloomFilter(expectedInsertion,falsePositiveTolerance);
+        String key = "HelloWorld";
+        bloomFilter.add(key.getBytes());
+        boolean contains = bloomFilter.mightContain(key.getBytes());
+        assertTrue(contains);
+        boolean mayNotContain = bloomFilter.mightContain("Hello".getBytes());
+        assertFalse(mayNotContain);
+
+
+    }
 }
