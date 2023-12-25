5dd24a49552c56513a7a95d387a2c5b41e1160cc
manick02 <manickavasagam.sundaram@gmail.com>
2023-12-25 17:58:29 +0530

    LSM Tree basic structure

--- hello-world/src/main/java/org/example/lsm/LSMTree.java
+++ hello-world/src/main/java/org/example/lsm/LSMTree.java
@@ -1,8 +1,31 @@
 package org.example.lsm;
 
-public class LSMTree {
+import static java.lang.Math.log;
 
-    public void add(byte[] bytes, byte[] bytes1) {
+public class LSMTree {
+    private int numLevels;
+    private Node sentinel;
+    public LSMTree(int numElements) {
+        this.numLevels = (int) Math.ceil(log(numElements)/log(2));
+        this.sentinel = new Node(null,null,numLevels);
+    }
+    public void add(byte[] key, byte[] value) {
         throw new RuntimeException("Not Implemented");
     }
+
+    public byte[] fetch(byte[] key) {
+        throw new RuntimeException("Not Implemented");
+    }
+
+    static class Node {
+        byte[] key;
+        byte[] value;
+        Node[] next;
+        Node(byte[] key, byte[] value,int numOfLevels) {
+            this.key = key;
+            this.value = value;
+            this.next = new Node[numOfLevels];
+        }
+    }
+
 }
